9fefbb82995cc78f59655e46e46e47a10abf747a
tomoyan99 <101825008+tomoyan99@users.noreply.github.com>
2023-11-23 00:03:25 +0900

    v1.0.0

--- resource/crypt.js
+++ resource/crypt.js
@@ -1,40 +1,28 @@
 "use strict";
 /* 
 	[crypt.js]
-	info.jsonの中身を暗号化および復号する
+	info.jsonの中身を暗号化するjsvascriptファイル
  */
 
-//cryptオブジェクトをエクスポート
-import {readFileSync} from "fs";
-import {hostname} from "os";
+const fs = require('fs');
 
-export const crypt = {
-	key : hostname(),
+//cryptオブジェクトをエクスポート
+exports.crypt = {
 	//文字列を1文字ずつ文字コードにして足し算
-	async strToDec(str) {
+	strToDec: async function strToDec(str) {
 		let codesum = 0x0;
 		for (let i = 0; i < str.length; i++) {
-			const charcode = str.slice(i, i + 1).charCodeAt(0) ;
+			const charcode = str.slice(i, i + 1).charCodeAt(0);
 			codesum += charcode;
 		}
 		return codesum;
 	},
 	//暗号化
-	async encrypt(data) {
-		const hirabun = JSON.stringify(data);
-		const dec = await this.strToDec(this.key);
+	encrypt: async function encrypt(hirabun, dec) {
 		let str = [];
 		str[0] = hirabun; //平文
-		let i, n;
-		let y = 0;
-		let x = dec;
-		//繰り返し回数をざっくり計算
-		for (n = 1; y < 4000000; n++) {
-			y = x * 2 * String(n).length
-			x = y;
-		}
-		// const dec2 = parseInt(dec / 35); //繰り返し回数
-		const dec2 = n - 1;//繰り返し回数
+		let i;
+		const dec2 = parseInt(dec / 35); //繰り返し回数
 		for (i = 0; i < dec2; i++) {
 			str[i + 1] = "";
 			for (let j = 0; j < str[i].length; j++) {
@@ -52,28 +40,21 @@ export const crypt = {
 			throw "暗号化エラー";
 		}
 	},
-	//復号
-	async decrypt(path) {
-		const angou = readFileSync(path, "utf-8");
-		const dec = await this.strToDec(this.key);
 
+	//復号
+	decrypt: async function decrypt(angou, dec) {
 		let str = [];
 		str[0] = angou;
-		let i, n;
-		let y = 0;
-		let x = dec;
-		//繰り返し回数を予めざっくり計算
-		for (n = 1; y < 4000000; n++) {
-			y = x * 2 * String(n).length
-			x = y;
-		}
-		// const dec2 = parseInt(dec / 35);
-		const dec2 = n - 1;//繰り返し回数
+		let i;
+		let count = [];
+		count[0] = 0;
+		const dec2 = parseInt(dec / 35); //繰り返し回数
 		for (i = 0; i < dec2; i++) {
 			str[i + 1] = "";
 			const ilen = (dec2 -1 - i).toString().length;
 			// console.log(ilen);
 			for (let j = 0; j < str[i].length / (ilen + 1) ; j++) {
+				count[j + 1] = ilen;
 				const start = j * (ilen+1);
 				const end = j * (ilen+1) + 1;
 
@@ -88,11 +69,10 @@ export const crypt = {
 			// console.log(str[i + 1] + "\n");
 		}
 		try {
-			return JSON.parse(str[i]);
+			return str[i];
 		} catch (e) {
 			console.log("復号に失敗しました");
 			throw "復号エラー";
 		}
 	}
 }
-
