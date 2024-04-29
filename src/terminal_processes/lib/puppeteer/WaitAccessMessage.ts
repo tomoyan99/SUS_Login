dc7b4e970f4ac110e259f12b3c9aa213071e7760
tomoyan99 <101825008+tomoyan99@users.noreply.github.com>
2024-04-30 02:05:18 +0900

    [v6.0.0/TS-dev1] jsをtsにリネームしたときにOpenerあたりのすでにtsにしてたやつらがバグった

---
+++ src/terminal_processes/lib/puppeteer/WaitAccessMessage.ts
@@ -0,0 +1,27 @@
+import {clearInterval} from "timers";
+process.stdout.setEncoding("utf8");
+class WaitAccessMessage{
+    private wait_msec:number;
+    private wait_access:NodeJS.Timeout;
+    private print_func:Function;
+    constructor(wait_msec:number,func:Function=console.log) {
+        this.wait_msec = wait_msec;
+        this.wait_access = undefined;
+        this.print_func = func;
+    }
+    //アクセスメッセージの開始
+    async consoleOn(prompt:string="アクセス中..."){
+        //アクセス待機メッセージ
+        this.wait_access = setInterval(()=>{
+            this.print_func(prompt);
+        },this.wait_msec);
+        return this.wait_access;
+    }
+    //アクセスメッセージの終了
+    async consoleOff(){
+        if (typeof this.wait_access){
+            clearInterval(this.wait_access)
+        }
+    }
+}
+export default WaitAccessMessage;
