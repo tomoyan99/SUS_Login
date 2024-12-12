40a5fc70115ae677780d959161fb368c414d10b6
tomoyan99 <101825008+tomoyan99@users.noreply.github.com>
2024-12-13 01:48:18 +0900

    [PIE_dev] TSに書き換えた

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import nodeExternals from "webpack-node-externals";
+import { Configuration, RuleSetRule} from "webpack"; // 型をインポート
+import pkj from "./package.json";
+
+const filename = `${pkj.name}_v${pkj.version}`;
+
+const config2: Configuration = {
+    module: {
+        rules: [
+            {
+                // 拡張子 .ts の場合
+                test: /\.ts$/,
+                // TypeScript をコンパイルする
+                use: "ts-loader",
+            } as RuleSetRule,
+        ],
+    },
+    resolve: {
+        // 拡張子を配列で指定
+        extensions: [".ts", ".js"],
+    },
+    entry: { main: path.resolve("SUS_Login_core/src/main/main.ts") },
+    output: {
+        filename: `[name].cjs`,
+        path: path.resolve(process.cwd(), "bundle/"),
+        library: {
+            type: "commonjs",
+        },
+        // chunkFormat や format の設定を追加
+    },
+    target: "node",
+    mode: "production",
+    // mode: "development",
+    externals: [<any>nodeExternals()], // Node.js モジュールを除外
+};
+
+export default config2;
